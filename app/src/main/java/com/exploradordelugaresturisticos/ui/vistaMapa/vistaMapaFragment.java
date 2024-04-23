5f36d86231bed0a01fd71db69d014e73ec539a6d
Fermin2049 <fermin2049@gmail.com>
2024-04-22 23:22:44 -0300

    agregado puntos de turismo

--- app/src/main/java/com/exploradordelugaresturisticos/ui/vistaMapa/vistaMapaFragment.java
+++ app/src/main/java/com/exploradordelugaresturisticos/ui/vistaMapa/vistaMapaFragment.java
@@ -4,19 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.exploradordelugaresturisticos.R;
-import com.exploradordelugaresturisticos.ui.lugares.LugaresViewModel;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -91,6 +87,25 @@ public class vistaMapaFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        // Lista de algunos lugares con sus latitudes y longitudes
+        LatLng lugar1 = new LatLng(-33.291958448935176, -66.31237669763469);
+        LatLng lugar2 = new LatLng(-33.28491899502414, -66.29938450784749);
+        LatLng lugar3 = new LatLng(-33.276218344724825, -66.33764389799929);
+        LatLng lugar4 = new LatLng(-33.30508500684787, -66.32341823345463);
+        LatLng lugar5 = new LatLng(-33.25882860782914, -66.24646435794719);   // Ejemplo: Moscú, Rusia
+
+        // Agregar marcadores en el mapa para cada lugar
+        map.addMarker(new MarkerOptions().position(lugar1).title("Parque de las Naciones"));
+        map.addMarker(new MarkerOptions().position(lugar2).title("Plaza del Cerro"));
+        map.addMarker(new MarkerOptions().position(lugar3).title("Villa Deportiva"));
+        map.addMarker(new MarkerOptions().position(lugar4).title("San Luis Shopping"));
+        map.addMarker(new MarkerOptions().position(lugar5).title("Monumento al Pueblo Puntano"));
+
+        // Además, puedes mover la cámara a una ubicación específica si lo deseas
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(lugar1, 10));
+
+        // Aquí también puedes iniciar las actualizaciones de ubicación si lo necesitas
         startLocationUpdates();
     }
 
